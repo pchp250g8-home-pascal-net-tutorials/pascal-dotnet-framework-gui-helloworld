85894a2563be7509aa10ee09ecc36dbe0868ef2d
Alexander <pchp250g8@tutamail.com>
2025-12-14 15:20:25 +0300

    HelloWorld

---
+++ Unit1.pas
@@ -0,0 +1,37 @@
+﻿Unit Unit1;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms;
+
+type
+  Form1 = class(Form)
+    procedure button1_Click(sender: Object; e: EventArgs);
+    procedure button2_Click(sender: Object; e: EventArgs);
+  {$region FormDesigner}
+  internal
+    {$resource Unit1.Form1.resources}
+    button1: Button;
+    button2: Button;
+    {$include Unit1.Form1.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+procedure Form1.button1_Click(sender: Object; e: EventArgs);
+begin
+  MessageBox.Show('Hello,World!!!','Hello');
+end;
+
+procedure Form1.button2_Click(sender: Object; e: EventArgs);
+begin
+  Application.Exit();
+end;
+
+end.
